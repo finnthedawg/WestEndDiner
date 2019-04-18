2fc5a58b8c033bedc580b73e806b66528f06b350
Bob Ross <42992404+finnthedog223@users.noreply.github.com>
2019-04-19 02:27:33 +0400

    functions to load menu from file

--- shared.cpp
+++ shared.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h> //atoi and other library functions
 #include "shared.h"
 
 /* Searches for ID in clientData array. Returns nullptr if not found */
@@ -11,7 +14,35 @@ struct clientData * getClientById(int Id, struct clientData* shmdata){
   return(nullptr);
 }
 
-/* Searches for itemID from our comma seperated menu. */
-/* Format: itemID,Description,Price,Min_time,Maxtime */
-char* findDescriptionMenu(int Id, char* path, struct item* items){
+/* Menu: itemID,Description,Price,Min_time,Maxtime */
+/* Searches for itemID from our  */
+char* findDescriptionMenu(int Id, std::vector<struct item> &menuList){
+}
+
+/*Adds a single item to our item vector*/
+void loadMenu(const char* filename, std::vector<struct item> &menuList){
+  FILE *fp = fopen(filename,"r");
+  char* itemLine = (char*)malloc(STRLEN);
+  if(fp != NULL){
+    while(fgets(itemLine, STRLEN, fp)){
+      menuList.push_back(createStruct(itemLine));
+    }
+  }
+  free(itemLine);
+  fclose(fp);
+
+}
+
+/*Creates a struct given a item line*/
+/*Format of itemline: itemID,Description,Price,Min_time,Maxtime */
+static struct item createStruct(char* itemLine){
+  struct item x;
+  const char delim = ',';
+  /*Parse the values into struct x */
+  x.itemId = atoi(strsep(&itemLine,&delim));
+  strcpy(x.description, strsep(&itemLine,&delim));
+  x.price = std::stof(strsep(&itemLine,&delim));
+  x.min_time = atoi(strsep(&itemLine,&delim));
+  x.max_time = atoi(strsep(&itemLine,&delim));
+  return(x);
 }
