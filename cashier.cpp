4ad86b9547bb4efa297306fc390f5229c4da751e
Bob Ross <42992404+finnthedog223@users.noreply.github.com>
2019-04-18 12:24:06 +0400

    Add cashier process

---
+++ cashier.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
+
+#include "shared.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+  /* Initialize our shared memory segment */
+  struct sharedData *shmdata; //Our data struct stored in shared memory
+  int shmid;
+  void *shmaddr; /* Pointer to head of shm*/
+
+  /* Load our previous SHM associated with SHMKEY*/
+  if ((shmid = shmget (SHMKEY, sizeof(struct sharedData), 0)) < 0){
+    perror("Shmget could not create segment");
+    exit(1);
+  }
+  D printf("Creating shared memory with SHMID: %d\n",shmid);
+
+  /* Attaches the shared memory with id shmid*/
+  if ((shmaddr = shmat(shmid, NULL, 0)) == (char *)-1){
+    perror("shmat could not attach");
+    exit(1);
+  }
+  D printf("Attached shared memory with SHMID: %d\n",shmid);
+
+  /* Convert shmaddr pointer to our data pointer. */
+  shmdata = (struct sharedData *)shmaddr;
+
+}
--- makefile
+++ makefile
@@ -1,13 +1,20 @@
 CFLAGS= -std=c++11
 
 all: 	coordinator \
-	client
+	client \
+	cashier
 
 coordinator: coordinator.o shared.o
 	g++ $(CFLAGS) coordinator.o shared.o -o coordinator -lpthread
 
 client: client.o shared.o
 	g++ $(CFLAGS) client.o shared.o -o client -lpthread
+
+cashier: cashier.o shared.o
+	g++ $(CFLAGS) cashier.o shared.o -o cashier -lpthread
+
+cashier.o: cashier.cpp shared.h
+	g++ $(CFLAGS) cashier.cpp -o cashier.o -c
 
 client.o: client.cpp shared.h
 	g++ $(CFLAGS) client.cpp -o client.o -c
@@ -19,4 +26,4 @@ shared.o: shared.cpp shared.h
 	g++ $(CFLAGS) shared.cpp -o shared.o -c
 
 clean:
-	rm *.o coordinator client
+	rm *.o coordinator client cashier
