4591e4eef5dc481a32d9484d6afd3ef16477b472
Bob Ross <42992404+finnthedog223@users.noreply.github.com>
2019-04-18 21:54:24 +0400

    Flag handling for the cashier

--- cashier.cpp
+++ cashier.cpp
@@ -6,6 +6,7 @@
 #include <sys/stat.h>        /* For mode constants */
 #include <semaphore.h>
 #include <unistd.h>
+#include <time.h>
 #include <string.h>
 
 #include "shared.h"
@@ -14,16 +15,45 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+  /* Some default cashier values */
+  int serviceTime = 10;
+  int breakTime = 10;
+  int shmid = -1; //-1 means using SHMKEY to generate the SHM rather than shmid
+
+  /* Parse our arguments */
+  int opt;
+  while((opt = getopt(argc, argv, "s:b:m:")) != -1){
+    switch(opt){
+      case 's':
+        printf("serviceTime set to %s\n", optarg);
+        serviceTime = stoi(optarg);
+        break;
+      case 'b':
+        printf("Max breakTime set to %s\n", optarg);
+        breakTime = stoi(optarg);
+        break;
+      case 'm':
+        printf("shmid set to %s\n", optarg);
+        shmid = stoi(optarg);
+        break;
+      case '?':
+        printf("Unknown flag: %s\n", optarg);
+        break;
+    }
+  }
+  srand(time(0)); //randomize with time seed
+  serviceTime = rand() % serviceTime;
+  breakTime = rand() % breakTime;
+
   /* Initialize our shared memory segment */
   struct sharedData *shmdata; //Our data struct stored in shared memory
-  int shmid;
 
   /* Load our previous SHM associated with SHMKEY*/
   if ((shmid = shmget (SHMKEY, sizeof(struct sharedData), 0)) < 0){
     perror("Shmget could not create segment");
     exit(1);
   }
-  D printf("Creating shared memory with SHMID: %d\n",shmid);
+  D printf("Obtaining shared memory with SHMID: %d\n",shmid);
 
   /* Attaches the shared memory with id shmid*/
   if ((shmdata = (struct sharedData *)shmat(shmid, NULL, 0)) == (struct sharedData *)-1){
@@ -57,15 +87,15 @@ int main(int argc, char *argv[]) {
       int pid = shmdata -> clientpid; //Remember this client.
       sem_post(&shmdata -> cashier_lock_sem); //Unlock cashiers and lets other cashiers call people.
 
-      printf("Servicing the client: %d\n", pid);
-      sleep(5); //Serve the client.
+      printf("Servicing the client: %d.\n", pid);
+      sleep(serviceTime); //Serve the client.
       sem_post(&getClientById(pid, shmdata->clients)->paid_sem);
       printf("Done servicing the client: %d\n",pid);
     } else {
       /* Otherwise, we take a break and unlock control.*/
       D printf("There is nobody in queue, taking a break. \n");
       sem_post(&shmdata -> cashier_lock_sem); //Unlock this cashier
-      sleep(5);
+      sleep(breakTime);
     }
 
   }
