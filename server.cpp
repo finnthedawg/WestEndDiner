a2b7821162b28b1c586bf5f10ffd8b50a01f0ffe
Bob Ross <42992404+finnthedog223@users.noreply.github.com>
2019-04-18 22:28:10 +0400

    To access SHM on server

--- server.cpp
+++ server.cpp
@@ -15,4 +15,26 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+  /* Some default server values */
+  int shmid = -1; //-1 means using SHMKEY to generate the SHM rather than shmid
+
+  /* Initialize our shared memory segment */
+  struct sharedData *shmdata; //Our data struct stored in shared memory
+
+  /* Load our previous SHM associated with SHMKEY*/
+  if ((shmid = shmget (SHMKEY, sizeof(struct sharedData), 0)) < 0){
+    perror("Shmget could not obtain segment");
+    exit(1);
+  }
+  D printf("Obtaining shared memory with SHMID: %d\n",shmid);
+
+  /* Attaches the shared memory with id shmid*/
+  if ((shmdata = (struct sharedData *)shmat(shmid, NULL, 0)) == (struct sharedData *)-1){
+    perror("shmat could not attach");
+    exit(1);
+  }
+
+
+
 }
