a3e79a423c33482c308f2b7898af933449ed8014
Bob Ross <42992404+finnthedog223@users.noreply.github.com>
2019-04-20 03:27:58 +0400

    Script to automatically populate restaurant with clients cashiers and server

---
+++ keepalive.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+"$@"
+exec "$SHELL"
+# Keeping terminal open https://askubuntu.com/questions/46627/how-can-i-make-a-script-that-opens-terminal-windows-and-executes-commands-in-the
---
+++ spawn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./cashier
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./cashier
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./server
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./client
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./client
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./client
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./client
+x-terminal-emulator -e /home/finn/Desktop/OS/diner/WestEndDiner/keepalive.sh ./client
+#Spawning and keeping terminals open https://askubuntu.com/questions/46627/how-can-i-make-a-script-that-opens-terminal-windows-and-executes-commands-in-the
